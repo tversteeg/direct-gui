6fb1658be79d5c751cc4bafd718607132be150da
Thomas Versteeg <thomasversteeg@gmx.com>
2018-01-27 14:28:49 +0100

    Bump version to 0.1.9 & add missing files

---
+++ build.rs
@@ -0,0 +1,36 @@
+//! Convert the default fonts in assets to a blitbuffer file which can be used by `default_font`.
+
+extern crate image;
+extern crate blit;
+
+use std::fs;
+
+use blit::*;
+
+fn save_blit_buffer_from_image(name: &str, mask_color: u32) {
+    let path = format!("assets/{}", name);
+
+    println!("Converting image \"{}\" to blit buffer", path);
+
+    let img = image::open(path).unwrap();
+    let img_as_rgb8 = match img.as_rgb8() {
+        Some(i) => i,
+        None => panic!("Could not convert image to RGB8 format")
+    };
+
+    let blit_buf = img_as_rgb8.to_blit_buffer(Color::from_u32(mask_color));
+
+    blit_buf.save(format!("resources/{}.blit", name)).unwrap();
+}
+
+fn main() {
+    fs::create_dir_all("resources").unwrap();
+
+    let asset_paths = fs::read_dir("assets").unwrap();
+
+    for path in asset_paths {
+        let filepath = path.unwrap().path();
+        let filename = filepath.file_name().unwrap();
+        save_blit_buffer_from_image(filename.to_str().unwrap(), 0xFF00FF);
+    }
+}
---
+++ examples/label.rs
@@ -0,0 +1,44 @@
+extern crate direct_gui;
+extern crate minifb;
+extern crate blit;
+
+use direct_gui::*;
+use direct_gui::controls::*;
+use minifb::*;
+use blit::Color;
+
+const WIDTH: usize = 400;
+const HEIGHT: usize = 120;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![0x222222; WIDTH * HEIGHT];
+
+    let mut window = Window::new("direct-gui label example - ESC to exit", WIDTH, HEIGHT, WindowOptions::default()).expect("Unable to open window");
+
+    let mut gui = Gui::new((WIDTH as i32, HEIGHT as i32));
+
+    let default_font = gui.default_font();
+    gui.register(Label::new(default_font).pos(10, 10).text("This is a label with a default font\nand a line break."));
+
+    let font = gui.load_font_sprite_from_file("assets/TorusSans.png", FontSettings {
+        start: '!',
+        char_size: (9, 9),
+        leading_offset: 2,
+        mask_color: Color::from_u32(0xFF00FF)
+    }).unwrap();
+    gui.register(Label::new(font).pos(10, 50).text("This is a label with a custom font."));
+
+    let font = gui.load_font_sprite_from_file("assets/TorusSansGradient.png", FontSettings {
+        start: '!',
+        char_size: (9, 9),
+        leading_offset: 2,
+        mask_color: Color::from_u32(0xFF00FF)
+    }).unwrap();
+    gui.register(Label::new(font).pos(10, 80).text("This is a label with a custom font\nand fancy colors."));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        gui.draw_to_buffer(&mut buffer);
+
+        window.update_with_buffer(&buffer).unwrap();
+    }
+}
